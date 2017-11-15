3ed7fcc8408a8f50a2cd54e1c56c30154510cc48
Ardenian <22526933+Ardenian@users.noreply.github.com>
2017-11-15 21:57:21 +0100

    adding textfield, moving game start

--- src/main/java/de/idrinth/gods_and_heroes/FXMLController.java
+++ src/main/java/de/idrinth/gods_and_heroes/FXMLController.java
@@ -1,21 +1,29 @@
 package de.idrinth.gods_and_heroes;
 
+import java.awt.TextField;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.Timer;
+
+import de.idrinth.gods_and_heroes.implementation.Player;
+import de.idrinth.gods_and_heroes.services.TaskHandler;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class FXMLController implements Initializable {
+	
+    private final Timer timer = new Timer(true);
     
     @FXML
     private Label label;
+    @FXML
+    private TextField textfield;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
+    	timer.scheduleAtFixedRate(new TaskHandler(new Player(textfield.getText(),null)), 0, 1000);
     }
     
     @Override
