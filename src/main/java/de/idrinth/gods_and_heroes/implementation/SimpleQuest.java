2efe778c7c26cf8ef7105fb7c1cd75a45ae01600
Björn Büttner <idrinth@users.noreply.github.com>
2017-11-17 19:18:50 +0100

    adding a simple quest implementation refs #1

---
+++ src/main/java/de/idrinth/gods_and_heroes/implementation/SimpleQuest.java
@@ -0,0 +1,59 @@
+package de.idrinth.gods_and_heroes.implementation;
+
+import de.idrinth.gods_and_heroes.interfaces.Option;
+import de.idrinth.gods_and_heroes.interfaces.Quest;
+import java.math.BigDecimal;
+import java.util.List;
+
+public class SimpleQuest implements Quest {
+    private final BigDecimal renown;
+    private BigDecimal progress = BigDecimal.ZERO;
+    private final BigDecimal difficulty;
+    private final List<Option> options;
+
+    public SimpleQuest(BigDecimal renown, BigDecimal difficulty, List<Option> options) {
+        this.renown = renown;
+        this.difficulty = difficulty;
+        this.options = options;
+    }
+
+    @Override
+    public BigDecimal getRenownReward() {
+        return renown;
+    }
+
+    @Override
+    public List<Option> getOptions() {
+        return options;
+    }
+
+    @Override
+    public void chooseOption(Option option) {
+        options.clear();
+        options.add(option);
+    }
+
+    @Override
+    public Option getChosenOption() {
+         return options.get(0);
+    }
+
+    @Override
+    public boolean isReady() {
+         return options.size() == 1;
+    }
+
+    @Override
+    public boolean isDone() {
+         return progress.compareTo(difficulty) >= 0;
+    }
+
+    @Override
+    public boolean addProgress(BigDecimal amount) {
+        if(options.size() != 1) {
+            return false;
+        }
+        progress = progress.add(amount);
+        return isDone();
+    }
+}
