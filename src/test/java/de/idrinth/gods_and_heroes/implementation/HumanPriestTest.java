2a66dccfd524b656bc7a5061c1097b480e0a8f1b
Björn Büttner <idrinth@users.noreply.github.com>
2017-11-17 19:08:13 +0100

    adding some more tests removing useless constructor

---
+++ src/test/java/de/idrinth/gods_and_heroes/implementation/HumanPriestTest.java
@@ -0,0 +1,82 @@
+package de.idrinth.gods_and_heroes.implementation;
+
+import de.idrinth.gods_and_heroes.interfaces.Alignment;
+import de.idrinth.gods_and_heroes.interfaces.Believer;
+import de.idrinth.gods_and_heroes.interfaces.God;
+import de.idrinth.gods_and_heroes.interfaces.Hero;
+import de.idrinth.gods_and_heroes.interfaces.Priest;
+import de.idrinth.gods_and_heroes.interfaces.Wonder;
+import java.math.BigDecimal;
+import javafx.collections.ObservableList;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HumanPriestTest {
+    @Test
+    public void testInitialization() {
+        System.out.println("initialization");
+        God god = new GodImpl();
+        HumanPriest priest = new HumanPriest(god);
+        assertNotNull(priest);
+        assertTrue(god.equals(priest.getGod()));
+        assertTrue(priest.getOnDeathBelieve().compareTo(BigDecimal.valueOf(25)) == 0);
+    }
+    private class GodImpl implements God {
+
+        @Override
+        public BigDecimal getBelieve() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public ObservableList<Believer> getBelievers() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public BigDecimal getRenown() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public boolean createWonder(Wonder wonder) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public ObservableList<Hero> getHeroes() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public ObservableList<Priest> getPriests() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public BigDecimal getLevel() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void addExperience(BigDecimal experience) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public Alignment getAlignment() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public String getName() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+
+        @Override
+        public void processIdle() {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+}
--- src/test/java/de/idrinth/gods_and_heroes/services/RandomStringArrayListTest.java
+++ src/test/java/de/idrinth/gods_and_heroes/services/RandomStringArrayListTest.java
@@ -1,9 +1,10 @@
 package de.idrinth.gods_and_heroes.services;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -23,15 +24,20 @@ public class RandomStringArrayListTest {
         }
         assertTrue(map.get("1")<600000);
         assertTrue(map.get("2")<600000);
-        
-        ArrayList<String> list = new ArrayList<String>();
+    }
+
+    @Test
+    public void testInitializationWithList() throws IOException {
+        System.out.println("initialization");
+        ArrayList<String> list = new ArrayList<>();
         list.add("a");
         list.add("b");
         list.add("c");
-        
-        instance = new RandomStringArrayList( list);
-        
-        instance = new RandomStringArrayList(10);
+        assertEquals(3, new RandomStringArrayList(list).size());
+        assertEquals(0, new RandomStringArrayList().size());
+        assertEquals(0, new RandomStringArrayList(new File("/will/never/exists/I/hope")).size());
+        File file = new File("./test");
+        FileUtils.writeStringToFile(file, "title\nfirst\nsecond", "ascii");// to be mocked
+        assertEquals(2, new RandomStringArrayList(file).size());
     }
-    
 }
