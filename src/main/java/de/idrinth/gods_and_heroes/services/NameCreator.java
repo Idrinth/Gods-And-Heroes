d494e84ebd90d51c2c3686c1c3a378a9f319b851
Ardenian <22526933+Ardenian@users.noreply.github.com>
2017-11-20 18:46:36 +0100

    adjusting generator version, implementing generator

--- src/main/java/de/idrinth/gods_and_heroes/services/NameCreator.java
+++ src/main/java/de/idrinth/gods_and_heroes/services/NameCreator.java
@@ -1,17 +1,10 @@
 package de.idrinth.gods_and_heroes.services;
 
-import java.util.ArrayList;
-import org.apache.commons.io.FileUtils;
+import de.idrinth.name_generator.API;
 
 public class NameCreator {
-    private static final RandomStringArrayList FIRST = new RandomStringArrayList(FileUtils.toFile(NameCreator.class.getResource("/names/CSV_Database_of_First_Names.csv")));
-    private static final RandomStringArrayList LAST = new RandomStringArrayList(FileUtils.toFile(NameCreator.class.getResource("/names/CSV_Database_of_Last_Names.csv")));
+	private static final API generator = new API();
     public static String generate() {
-        ArrayList<String> parts = new ArrayList<>();
-        for(int i= (int) Math.ceil(Math.random()*3);i>=0;i--) {
-            parts.add(FIRST.get());
-        }
-        parts.add(LAST.get()+(Math.random()<0.2?"-"+LAST.get():""));
-        return String.join(" ", parts);
+    	return generator.makeName();
     }
 }
