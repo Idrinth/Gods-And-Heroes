e31e4f2b68aeeb08fe55759b29ec005ffbf912e4
Björn Büttner <Idrinth@users.noreply.github.com>
2017-12-02 01:58:09 +0100

    fixes #31

--- src/test/java/de/idrinth/gods_and_heroes/ui/AttributeItemTest.java
+++ src/test/java/de/idrinth/gods_and_heroes/ui/AttributeItemTest.java
@@ -10,7 +10,7 @@ public class AttributeItemTest {
     public void testSetValue() {
         System.out.println("getValue + setValue");
         assertEquals("0", new AttributeItem("ZZZ").getValue());
-        AttributeItem instance = new AttributeItem("Q", BigInteger.ONE);
+        AttributeItem instance = new AttributeItem("Q", BigInteger.ONE, "");
         assertEquals("1", instance.getValue());
         instance.setValue(BigInteger.TEN);
         assertEquals("10", instance.getValue());
@@ -20,8 +20,21 @@ public class AttributeItemTest {
     public void testGetName() {
         System.out.println("getName");
         assertEquals("A", new AttributeItem("A").getName());
-        assertEquals("A", new AttributeItem("A", BigInteger.ONE).getName());
+        assertEquals("A", new AttributeItem("A", "").getName());
+        assertEquals("A", new AttributeItem("A", BigInteger.ONE, "").getName());
         assertEquals("ZZZ", new AttributeItem("ZZZ").getName());
-        assertEquals("ZZZ", new AttributeItem("ZZZ", BigInteger.TEN).getName());
+        assertEquals("ZZZ", new AttributeItem("ZZZ", "").getName());
+        assertEquals("ZZZ", new AttributeItem("ZZZ", BigInteger.TEN, "").getName());
+    }
+
+    @Test
+    public void testGetTooltip() {
+        System.out.println("getTooltip");
+        assertEquals("", new AttributeItem("A").getTooltip());
+        assertEquals("A", new AttributeItem("", "A").getTooltip());
+        assertEquals("A", new AttributeItem("", BigInteger.ONE, "A").getTooltip());
+        assertEquals("", new AttributeItem("ZZZ").getTooltip());
+        assertEquals("ZZZ", new AttributeItem("", "ZZZ").getTooltip());
+        assertEquals("ZZZ", new AttributeItem("", BigInteger.TEN, "ZZZ").getTooltip());
     }
 }
