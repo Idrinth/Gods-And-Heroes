03d6381c287c9c8ec7ac77fed1e464bcbd8ccace
Ardenian <22526933+Ardenian@users.noreply.github.com>
2017-12-03 16:59:47 +0100

    populating quest factory

--- src/main/java/de/idrinth/gods_and_heroes/services/QuestFactory.java
+++ src/main/java/de/idrinth/gods_and_heroes/services/QuestFactory.java
@@ -1,9 +1,20 @@
 package de.idrinth.gods_and_heroes.services;
 
-import de.idrinth.gods_and_heroes.interfaces.Quest;
+import java.math.BigDecimal;
+import java.util.List;
+
+import de.idrinth.gods_and_heroes.implementation.*;
+import de.idrinth.gods_and_heroes.interfaces.*;
 
 public class QuestFactory {
-    public Quest generate() {
-        throw new UnsupportedOperationException("Not supported yet.");
+    public static Quest generate(BigDecimal renownReward,List<Option> options) {
+        int number = (int) Math.random()*100;
+        if(number < 15) {
+        	return new SetbackQuest(renownReward, options, (int)Math.random()*25, renownReward.divide(BigDecimal.TEN));
+        }
+        if(number < 35) {
+        	return new FailQuest(renownReward, options, (int)Math.random()*75);
+        }
+        return new SimpleQuest(renownReward, options);
     }
 }
